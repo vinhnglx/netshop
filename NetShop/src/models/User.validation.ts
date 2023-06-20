8f3aa147977c11b3ef32e66fbcc133adeb19c7c5
Vinh Nguyen <vinh.nglx@gmail.com>
2023-06-20 15:40:21 +1200

    Authentication: Create User validation file, it includes - userSchema to validate firstName, lastName, username, password, email.
    Validation includes:
    + swear words check for firstName, lastName, username.
    + email format
    + username must be lowercase
    + username should only include letters, numbers, and underscores (_)
    + password must be strong - following NIST guidleline

---
+++ NetShop/src/models/User.validation.ts
@@ -0,0 +1,88 @@
+import {Profanity, ProfanityOptions} from '@2toad/profanity';
+import {PasswordStrengthStatistics} from 'tai-password-strength';
+import {object, string} from 'yup';
+
+const options = new ProfanityOptions();
+options.wholeWord = false;
+const profanity = new Profanity(options);
+profanity.addWords(['b00b', 'wh0re', 'wh0r3', 'whor3', 'a55h01e']); // TODO: Add more words
+
+const taiPasswordStrength = require('tai-password-strength');
+
+export enum PasswordStrengthCode {
+  VERY_WEAK = 'VERY_WEAK',
+  WEAK = 'WEAK',
+  REASONABLE = 'REASONABLE',
+  STRONG = 'STRONG',
+  VERY_STRONG = 'VERY_STRONG',
+}
+
+export const userSchema = object({
+  firstName: string()
+    .required()
+    .test({
+      test(value, ctx) {
+        if (profanity.exists(value)) {
+          return ctx.createError({
+            message: 'First Name should not contain swear words',
+          });
+        }
+        return true;
+      },
+    }),
+  lastName: string()
+    .required()
+    .test({
+      test(value, ctx) {
+        if (profanity.exists(value)) {
+          return ctx.createError({
+            message: 'Last Name should not contain swear words',
+          });
+        }
+        return true;
+      },
+    }),
+  username: string()
+    .required()
+    .min(3)
+    .lowercase()
+    .matches(/^[a-zA-Z0-9_]+$/)
+    .test({
+      test(value, ctx) {
+        if (profanity.exists(value)) {
+          return ctx.createError({
+            message: 'User Name should not contain swear words',
+          });
+        }
+        return true;
+      },
+    }),
+  email: string().email(),
+  password: string()
+    .required()
+    .test({
+      test(value, ctx) {
+        // Check the password against the password strength library
+        const passwordStrength = new taiPasswordStrength.PasswordStrength();
+        const passwordStrengthResult: PasswordStrengthStatistics =
+          passwordStrength.check(value);
+
+        switch (passwordStrengthResult.strengthCode) {
+          case PasswordStrengthCode.VERY_WEAK:
+          case PasswordStrengthCode.WEAK:
+            return ctx.createError({
+              message: 'Password is not strong enough',
+            });
+          case PasswordStrengthCode.REASONABLE:
+          case PasswordStrengthCode.STRONG:
+          case PasswordStrengthCode.VERY_STRONG:
+            return true;
+        }
+      },
+    }),
+}).required();
+
+export const signInSchema = object({
+  username: string().required(),
+  password: string().required(),
+}).required();
