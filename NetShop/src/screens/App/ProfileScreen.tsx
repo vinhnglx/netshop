bf4a46bb738946ebf0142d3983c0effbb530f6c4
Vinh Nguyen <vinh.nglx@gmail.com>
2023-06-23 22:58:01 +1200

    App: Update ProfileScreen to show which username logged in

--- NetShop/src/screens/App/ProfileScreen.tsx
+++ NetShop/src/screens/App/ProfileScreen.tsx
@@ -1,20 +1,31 @@
-import {Button} from '@rneui/themed';
+import {Button, Text} from '@rneui/themed';
 import React from 'react';
 import {StyleSheet, View} from 'react-native';
 import {useAuth} from '../../hooks/useAuth';
+import {useGetUser} from '../../hooks/useGetUser';
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#F8F9FA',
+    paddingHorizontal: 20,
+    paddingVertical: 20,
+  },
+  userName: {
+    marginBottom: 20,
   },
 });
 
 const ProfileScreen = () => {
-  const auth = useAuth();
+  const {authResponse, signOut} = useAuth();
+  const user = useGetUser(authResponse?.userId);
+
   return (
     <View style={styles.container}>
-      <Button color="warning" onPress={async () => auth.signOut()}>
+      <Text h4 h4Style={styles.userName}>
+        User name: {user.data?.username}
+      </Text>
+      <Button color="warning" onPress={async () => signOut()}>
         Log out
       </Button>
     </View>
